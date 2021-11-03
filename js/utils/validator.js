f76a924babe14dab3c3b8ec05467dc4b78cbc955
jibaru <xxnacho1997xx@gmail.com>
2021-11-03 14:07:49 -0500

    Created validator.js

---
+++ js/utils/validator.js
@@ -0,0 +1,52 @@
+export class InputValidator {
+  static MESSAGE = "Input inválido";
+
+  constructor(input) {
+    this._input = input;
+  }
+
+  isValid() {
+    const isString = (obj) =>
+      Object.prototype.toString.call(obj) === "[object String]";
+    const isNum = (str) => /^\d+$/.test(str);
+
+    if (!isString(this._input)) {
+      return false;
+    }
+
+    let isInvalid = false;
+
+    const splittedParts = this._input
+      .trim()
+      .split("\n")
+      .map((text) => {
+        if (isInvalid) return;
+
+        const parts = text.split(",");
+
+        if (parts.length != 3) {
+          isInvalid = true;
+        }
+
+        const [name, tll, raf] = parts;
+
+        if (!isNum(tll) || !isNum(raf)) {
+          isInvalid = true;
+        }
+
+        return { name, tll: parseInt(tll), raf: parseInt(raf) };
+      });
+
+    if (splittedParts.length == 0 || isInvalid) {
+      return false;
+    }
+
+    for (const part of splittedParts) {
+      if (part.name == undefined || isNaN(part.tll) || isNaN(part.raf)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}

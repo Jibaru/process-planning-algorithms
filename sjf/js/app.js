367098616b8c3a50ef0b507332c344b907bc3423
jibaru <xxnacho1997xx@gmail.com>
2021-11-03 14:08:35 -0500

    Refactored with validator and parser

--- sjf/js/app.js
+++ sjf/js/app.js
@@ -3,7 +3,8 @@ import { TableAvgResponse } from "../../js/tables/table_average_response.js";
 import { TableAvgWaitTime } from "../../js/tables/table_average_wait_time.js";
 import { TableGant } from "../../js/tables/table_gant.js";
 import { TableProccesses } from "../../js/tables/table_proccesses.js";
-
+import { InputParser } from "../../js/utils/parser.js";
+import { InputValidator } from "../../js/utils/validator.js";
 import { SJFQueue } from "../../js/structures/sjf_queue.js";
 
 const FifoForm = {
@@ -20,22 +21,27 @@ const FifoForm = {
     this.formInput.addEventListener("submit", (e) => {
       e.preventDefault();
       this.clearTables();
-      const proccesses = this.getProccesses(e);
 
-      if (proccesses) {
+      if (this.validInput(e)) {
+        const proccesses = this.getProccesses(e);
         this.results.style.display = "block";
         this.fillTables(proccesses);
+      } else {
+        this.results.style.display = "none";
+        alert(InputValidator.MESSAGE);
       }
     });
   },
 
-  getProccesses(e) {
-    const proccesses = e.target[0].value.split("\n").map((text) => {
-      const [name, tll, raf] = text.split(",");
-      return new Proccess(name, parseInt(tll), parseInt(raf));
-    });
+  validInput(e) {
+    const input = e.target[0].value;
+    return new InputValidator(input).isValid();
+  },
 
-    return proccesses;
+  getProccesses(e) {
+    return new InputParser(e.target[0].value).parse().map((arr) => {
+      return new Proccess(...arr);
+    });
   },
 
   fillTables(proccesses) {

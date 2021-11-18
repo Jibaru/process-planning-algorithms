b2ca966a24caa44fdc567beeba7725c91ff64063
jibaru <xxnacho1997xx@gmail.com>
2021-11-17 21:44:18 -0500

    Created GatnItem structure

---
+++ js/structures/gant_item.js
@@ -0,0 +1,7 @@
+export class GantItem {
+  constructor(proccess, initialTime, finalTime) {
+    this.proccess = proccess;
+    this.initialTime = initialTime;
+    this.finalTime = finalTime;
+  }
+}

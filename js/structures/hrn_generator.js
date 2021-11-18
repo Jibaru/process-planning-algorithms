06deb238e91eb60d912fffae656036d307a42e23
jibaru <xxnacho1997xx@gmail.com>
2021-11-17 19:17:05 -0500

    Created hrn generator

---
+++ js/structures/hrn_generator.js
@@ -0,0 +1,54 @@
+export class HRNGenerator {
+  constructor(proccesses) {
+    this._proccessQueue = [];
+    this._index = 0;
+    this._timeExcceeded = 0;
+    this._tllMin = Math.min(...proccesses.map((p) => p.tll));
+    proccesses.forEach((p) => this.push(p));
+  }
+  get timeExcceeded() {
+    return this._timeExcceeded;
+  }
+  push(proccess) {
+    this._index++;
+    this._proccessQueue.push({
+      proccess,
+      index: this._index,
+    });
+  }
+  pop() {
+    const proccessesFinded = this._proccessQueue.filter(
+      (p) =>
+        p.proccess.tll == this._tllMin || p.proccess.tll <= this._timeExcceeded
+    );
+    if (proccessesFinded.length == 1) {
+      this._timeExcceeded += proccessesFinded[0].proccess.raf;
+      const i = this._proccessQueue.findIndex(
+        (p) => p.index == proccessesFinded[0].index
+      );
+      this._proccessQueue.splice(i, 1);
+      return proccessesFinded[0].proccess;
+    }
+    const priorityProccesses = proccessesFinded.map((p) => {
+      return {
+        indexedProccess: p,
+        priority: this.priority(p.proccess),
+      };
+    });
+    const proccessMaxPriority = priorityProccesses.reduce((prev, current) => {
+      return prev.priority > current.priority ? prev : current;
+    });
+    this._timeExcceeded += proccessMaxPriority.indexedProccess.proccess.raf;
+    const i = this._proccessQueue.findIndex(
+      (p) => p.index == proccessMaxPriority.indexedProccess.index
+    );
+    this._proccessQueue.splice(i, 1);
+    return proccessMaxPriority.indexedProccess.proccess;
+  }
+  isEmpty() {
+    return this._proccessQueue.length == 0;
+  }
+  priority(p) {
+    return (this._timeExcceeded - p.tll + p.raf) / p.raf;
+  }
+}
